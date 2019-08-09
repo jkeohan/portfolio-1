e9eb3d42daae4580f8aa90dfc24552bf46495b50
Eloise Barrow <eloisebarrow@Eloises-MacBook-Air.local>
2019-08-09 10:27:08 -0400

    Updated hamburger styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 /*********** VARIABLES ***************/
-let hamburgerMenu = document.getElementById('hamburger-menu');
-console.log(hamburgerMenu)
+const hamburgerMenu = document.getElementById('hamburger-menu');
 
+// when hamburgerMenu is clicked, style each nav-item to be right-aligned
+// nav-item container should be width: 50%
+// style #hamburger-bio
 
-
-
-
+hamburgerMenu.addEventListener('click', updateHamburgerStyles);
 
 
 
@@ -13,7 +13,18 @@ console.log(hamburgerMenu)
 
 /*********** EVENT LISTENERS ***************/
 
+function updateHamburgerStyles() {
+  const hamburgerBio = document.getElementById('hamburger-bio');
+  const navLinks = document.getElementsByClassName('nav-link');
 
+  hamburgerBio.style.padding = '5% 15%';
+  // hamburgerBio.style.textAlign = 'center';
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].style.textAlign = 'right';
+    navLinks[i].background = 'rgb(0,0,0,0.1';
+  }
+  // console.log('this is updateHamburgerStyles')
+}
 
 
 
