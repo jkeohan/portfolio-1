019a8ce734a60dd92fd1e66feee02c4f46ccf578
Eloise Barrow <eloisebarrow@Eloises-MacBook-Air.local>
2019-08-09 10:03:45 -0400

    Added mobile logo and additional hamburger menu styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+/*********** VARIABLES ***************/
+let hamburgerMenu = document.getElementById('hamburger-menu');
+console.log(hamburgerMenu)
 
+
+
+
+
+
+
+
+
+
+/*********** EVENT LISTENERS ***************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*********** FUNCTION DEFINITIONS ***************/
